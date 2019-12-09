dd317983e952b6c76e1386fa9cb2cfad43a270e7
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-12-09 13:55:38 -0800

    Handle inputs and outputs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import * as core from '@actions/core'
+import * as path from 'path'
 
 async function run(): Promise<void> {
   try {
-    core.getInput('milliseconds')
+    const inputPath = core.getInput('inputPath')
+    const outputPath =
+      core.getInput('outputPath') ||
+      `${inputPath.substring(0, inputPath.length - path.extname(inputPath).length)}.md`
+
+    core.setOutput('outputPath', outputPath)
   } catch (error) {
     core.setFailed(error.message)
   }
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,3 +1,32 @@
-test('placeholder', async () => {
-  await expect(1).toBe(1)
+import * as process from 'process'
+import * as cp from 'child_process'
+import * as path from 'path'
+
+describe('generate-markdown-table', () => {
+  it('generates a default outputPath from the inputPath', () => {
+    process.env['INPUT_INPUTPATH'] = '/foo/bar/baz.json'
+
+    const ip = path.join(__dirname, '..', 'lib', 'main.js')
+    const options: cp.ExecSyncOptions = {
+      env: process.env
+    }
+
+    const output = cp.execSync(`node ${ip}`, options).toString()
+
+    expect(output).toMatch('::set-output name=outputPath,::/foo/bar/baz.md')
+  })
+
+  it('uses the outputPath when specified', () => {
+    process.env['INPUT_INPUTPATH'] = '/foo/bar/baz.json'
+    process.env['INPUT_OUTPUTPATH'] = `/alice/bob/clarice.md`
+
+    const ip = path.join(__dirname, '..', 'lib', 'main.js')
+    const options: cp.ExecSyncOptions = {
+      env: process.env
+    }
+
+    const output = cp.execSync(`node ${ip}`, options).toString()
+
+    expect(output).toMatch('::set-output name=outputPath,::/alice/bob/clarice.md')
+  })
 })
