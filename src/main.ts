457d2baa33e6f43ae78fbe881b075b7e939669f5
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-12-10 09:20:42 -0800

    Add debugging statements, just in case

--- src/main.ts
+++ src/main.ts
@@ -12,13 +12,19 @@ const writeFile = util.promisify(fs.writeFile)
 async function run(): Promise<void> {
   try {
     const inputPath = core.getInput('inputPath')
-    core.debug(inputPath)
+    core.debug(`Input path: ${inputPath}`)
+
     const outputPath =
       core.getInput('outputPath') ||
       `${inputPath.substring(0, inputPath.length - path.extname(inputPath).length)}.md`
+    core.debug(`Output path: ${outputPath}`)
 
     const input = JSON.parse(await readFile(inputPath, 'utf8'))
+    core.debug(`Input: ${input}`)
+
     const output = markdownTable(input.data, input.options || {})
+    core.debug(`Output: ${output}`)
+
     await writeFile(outputPath, output)
 
     core.setOutput('outputPath', outputPath)
